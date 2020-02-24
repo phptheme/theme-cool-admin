0eb0e355ba465466bca2833c6d13d6614e2cc171
phptheme <dev@getphptheme.com>
2020-02-24 22:47:19 +0300

    n/a

--- MainMenuItem.php
+++ MainMenuItem.php
@@ -5,9 +5,7 @@ namespace PhpTheme\Themes\CoolAdmin;
 class MainMenuItem extends \PhpTheme\Bootstrap4\MenuItem
 {
 
-    const SUBMENU = MainMenu::class;
-
-    public $submenuOptions = [
+    public $menuOptions = [
         'attributes' => [
             'class' => 'header3-sub-list list-unstyled'
         ]
